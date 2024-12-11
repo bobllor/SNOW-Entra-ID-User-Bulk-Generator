f2d8953b8a410c2386e07cc890741c1d801eec47
bobllor <bobllor@yahoo.com>
2024-12-11 14:59:35 -0600

    added in the placeholders for the manual input option

--- support/menu.py
+++ support/menu.py
@@ -1,19 +1,26 @@
 from typing import Iterable
+import os
 
-def format_text(text: str) -> str:
+def format_text(text: str) ->  str:
     return f'\t{text}'
 
 def menu():
     print(format_text('\nSelect an option:\n'))
     print(format_text('[m] Upload multiple files'))
     print(format_text('[s] Upload a single file'))
+    print(format_text('[i] Manual Input'))
     print(format_text('[q] Quit'))
 
 def choice(valid_options: Iterable[str]) -> str:
+    error = False
     while True:
-        option = input(format_text('\nEnter an option: ')).lower()
+        text = 'Enter an option: ' if not error else 'ERROR: Option not found. Enter a valid option: '
 
+        menu()
+        option = input(format_text('\n' + text)).lower()
+        
         if option not in valid_options:
-            print(format_text('ERROR: Option not found.'))
+            error = True
+            os.system('clear')
         else:
             return option
