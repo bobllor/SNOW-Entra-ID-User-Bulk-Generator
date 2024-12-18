94a0dced6f14fc59b648a176c132918d33716ef1
bobllor <bobllor@yahoo.com>
2024-12-18 15:52:09 -0600

    changed format_text logic.

--- support/menu.py
+++ support/menu.py
@@ -1,15 +1,15 @@
 from typing import Iterable
 import os
 
-def format_text(text: str) ->  str:
-    return f'\t{text}'
+def format_text(text: str) ->  None:
+    print(f'\t{text}')
 
 def menu():
-    print(format_text('\nSelect an option:\n'))
-    print(format_text('[m] Upload multiple files'))
-    print(format_text('[s] Upload a single file'))
-    print(format_text('[i] Manual Input'))
-    print(format_text('[q] Quit'))
+    format_text('\nSelect an option:\n')
+    format_text('[m] Upload multiple files')
+    format_text('[s] Upload a single file')
+    format_text('[i] Manual Input')
+    format_text('[q] Quit')
 
 def choice(valid_options: Iterable[str]) -> str:
     error = False
@@ -17,7 +17,7 @@ def choice(valid_options: Iterable[str]) -> str:
         text = 'Enter an option: ' if not error else 'ERROR: Option not found. Enter a valid option: '
 
         menu()
-        option = input(format_text('\n' + text)).lower()
+        option = input(f'\n\t{text}').lower()
         
         if option not in valid_options:
             error = True
