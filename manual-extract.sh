fa0acfef04dbf80932580ddd23867928eabcc4a5
bobllor <bobllor@yahoo.com>
2025-05-30 15:44:40 -0500

    Readded in file moving command

--- manual-extract.sh
+++ manual-extract.sh
@@ -11,6 +11,9 @@ if [[ -e $temp_name ]]; then
 	rm $temp_name
 fi
 
-headers=$(find -name "*.csv" -print -quit | xargs cat | grep -E "(version|Name|First)" > $temp_name)
+# move text files out
+find -type f -name "*.txt" | while read line; do mv "$line" $(pwd); done
 
+# csv generation
+find -name "*.csv" -print -quit | xargs cat | grep -E "(version|Name|First)" > $temp_name
 find -type f -name "*.csv"  ! -name $temp_name | while read line; do cat "$line" | grep -Ev "(version|Name|First)" >> $temp_name; done
